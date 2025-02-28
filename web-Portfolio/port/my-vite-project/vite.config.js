1691a835043b2d2b02e7896854df4b0830045078
Rafael Vieira <rafa.pinto.vieira@gmail.com>
2025-02-28 15:47:58 +0000

    Just study folders

--- web-Portfolio/port/my-vite-project/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import denoPlugin from '@deno/vite-plugin';
-
-export default defineConfig({
-  plugins: [denoPlugin()],
-  // Your Vite configuration here
-});
